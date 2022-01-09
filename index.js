3130d11dd8876a9bb1de06376f593b37ed244944
Yeohoon <yhoon.yoon@gmail.com>
2022-01-09 16:09:52 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
+  // Write your code here!
+  if ( feet <= 400){
+    return 'This one is on me!';
+  }
+  if (feet >= 2000 && feet < 2501){
+    return `I will gladly take your thirty bucks.`
+  }else{
+    return "No can do.";
+  }
+}
+
+
+function ternaryCheckCity(city){
+  return city === "NYC" ? "Ok, sounds good." : "No go."
   // Write your code here!
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(word){
   // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+  switch (word){
+    case "generous":
+      return 'Thank you so much.';
+    case "not as generous":
+      return 'Thank you.';
+    default: 
+      return 'Bye.';
+  }
 }
